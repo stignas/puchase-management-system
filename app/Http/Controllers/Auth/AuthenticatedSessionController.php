9649a02ad095683c1f38f8a330d9ba1b628c334c
Ignas <statkus.online@gmail.com>
2023-02-27 01:06:33 +0200

    AuthenticatedSessionController->create view changed from 'dashboard' to 'home'

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -17,7 +17,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('dashboard');
+        return view('home');
     }
 
     /**
